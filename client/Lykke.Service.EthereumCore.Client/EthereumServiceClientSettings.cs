12e11b72255ce6a6baa8ed0375a27e151aa340be
atarutin <tarutin_andrew@mail.ru>
2017-11-24 13:17:55 +0300

    added client settings

---
+++ client/Lykke.Service.EthereumCore.Client/EthereumServiceClientSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.EthereumCoreClient
+{
+    public class EthereumServiceClientSettings
+    {
+        public string ServiceUrl { get; set; }
+        public string HotwalletAddress { get; set; }
+    }
+}
